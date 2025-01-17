1ada9494d5e11e1beca6b97e918f1ee4980daff9
Rodrigo Alves <r.alves2505@hotmail.com>
2025-01-16 21:25:11 -0300

    Modificando e simplificando o programa 8 da pasta Elixir

--- Programas em Elixir/programa8.ex
+++ Programas em Elixir/programa8.ex
@@ -1,16 +1,4 @@
 defmodule ExpressaoQuadratica do
-  defmodule Multiplique do
-    def produto(p, x) do
-      p*x
-    end
-  end
-end
-defmodule Quadrado do
-  def quadrado(x) do
-    x**2
-  end
-end
-defmodule ExpressaoQuadratica.Multiplique.Quadrado do
   def combinacao(x, p) do
     x**2 + p*x + p**2
   end
