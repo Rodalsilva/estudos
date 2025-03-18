bb9adeba0e3c2586063ad3761de7d3f7c68c4208
Rodrigo Alves <r.alves2505@hotmail.com>
2025-03-18 02:28:10 -0300

    Finalizado o programa i_8 na pasta Python

--- Programas em Python/Matemática Comercial/programa_i_8.py
+++ Programas em Python/Matemática Comercial/programa_i_8.py
@@ -1,1 +1,32 @@
+# Programa que calcula as variações percentuais sucessiva a partir de um dicionário
+
+# Programa que calcula os valores das variações percentuais sucessivas.
+
+tabela = {"Julho": 298,
+          "Agosto": 329,
+          "Setembro": 283,
+          "Outubro": 319,
+          "Novembro": 302}
+v = list(tabela.values())
+n = len(v)
+x = 0
+j = [0, 0]
+while x < n:
+    if x > 0:
+        j[x] = (v[x]/v[x-1])-1
+        if x == n-1:
+            j[x] = (v[x]/v[x-1])-1
+            break
+        else:
+            j.append(0)
+    x = x + 1
+u = list(tabela.keys())
+z = 0
+while z < len(u)-1:
+    print(f"A variação percentual entre {u[z]} e {u[z+1]} é {j[z+1]: 5.2f}")
+    z = z + 1
+
+# Acima, utilizamos tabela.values() e tabela.keys() para obtermos apenas, em cada objeto,
+# as sequências de valores e chaves, respectivamente.
+# A função list transforma o objeto dado em seu parâmetro em uma lista.
 
--- Programas em Python/teste.py
+++ Programas em Python/teste.py
@@ -1,5 +1,28 @@
 
 
+tabela = {"Julho": 298,
+          "Agosto": 329,
+          "Setembro": 283,
+          "Outubro": 319,
+          "Novembro": 302}
+v = list(tabela.values())
+n = len(v)
+x = 0
+j = [0, 0]
+while x < n:
+    if x > 0:
+        j[x] = (v[x]/v[x-1])-1
+        if x == n-1:
+            j[x] = (v[x]/v[x-1])-1
+            break
+        else:
+            j.append(0)
+    x = x + 1
+u = list(tabela.keys())
+z = 0
+while z < len(u)-1:
+    print(f"A variação percentual entre {u[z]} e {u[z+1]} é {j[z+1]: 5.2f}")
+    z = z + 1
 
 
 
