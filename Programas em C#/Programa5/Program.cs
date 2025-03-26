020db297a877ef5ce42a36635907b1c351d5ed39
Rodrigo Alves <r.alves2505@hotmail.com>
2025-03-26 04:49:10 -0300

    Criado o programa temperatura em Portugol

---
+++ Programas em C#/Programa5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c;
+            a = 5;
+            b = 2;  
+            c = (double) a / b;
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
